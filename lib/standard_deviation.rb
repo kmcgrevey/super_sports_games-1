1ebc9a3474759f487789d8d49f336cab3ee0b021
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-21 13:58:49 -0700

    Calculates mean

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -4,7 +4,13 @@ ages = [24, 30, 18, 20, 41]
 
 # When you find the standard deviation, print it out
 sum = 0
-ages.each do |sum|
-    @sum += sum
-end
- puts @sum
+# ages.each do |sum|
+#     @sum += sum
+# end
+# array_sum = ages.each { |a| sum+=a }
+array_sum = 133 # <---- 133 MAKE IT DO IT!!
+# puts array_sum
+array_count = ages.length
+# puts array_count
+mean = (array_sum).to_f / array_count # <---- 26.6
+puts mean
