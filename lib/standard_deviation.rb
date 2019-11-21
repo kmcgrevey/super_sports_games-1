97169e426ffef157dfd6be73bc7c06e4c12ebcc3
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-21 14:57:17 -0700

    Complete array_sum variable

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -3,12 +3,14 @@ ages = [24, 30, 18, 20, 41]
 # Your code here for calculating the standard deviation
 
 # When you find the standard deviation, print it out
-sum = 0
-# ages.each do |sum|
-#     @sum += sum
-# end
+array_sum = 0
+ages.each do |x|
+    array_sum += x
+    # require "pry"; binding.pry
+end
+puts array_sum
 # array_sum = ages.each { |a| sum+=a }
-array_sum = 133 # <---- 133 MAKE IT DO IT!!
+# array_sum = 133 # <---- 133 MAKE IT DO IT!!
 # puts array_sum
 array_count = ages.length
 # puts array_count
@@ -23,4 +25,4 @@ puts mean
 step6 = 343.2
 # standard_deviation = Math.sqrt(step6 / array_count)
 standard_deviation = Math.sqrt(step6 / array_count).round(2)
-puts standard_deviation
+puts "#{standard_deviation} <---- YOUR STANDARD DEVIATION"
